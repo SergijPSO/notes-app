2a817c8d154d32c2f4535944bce15cd492439d04
Sergij Pankiv <pankivsergij97@gmail.com>
2022-12-30 18:54:15 +0200

    Data sync was added on using app in diff tabs

--- notes-app.js
+++ notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
 	searchText: ''
@@ -13,16 +13,25 @@ document.querySelector('#create-note').addEventListener('click', function(e) {
 		title: '',
 		body: ''
 	})
+
 	saveNotes(notes)
 	renderNotes(notes, filters)
 	location.assign(`/edit.html#${id}`)
 })
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+document.querySelector('#search-text').addEventListener('input', function(e) {
 	filters.searchText = e.target.value
 	renderNotes(notes, filters)
 })
 
-document.querySelector('#filter-by').addEventListener('change', function (e) {
+document.querySelector('#filter-by').addEventListener('change', function(e) {
 	console.log(e.target.value)
+})
+
+
+window.addEventListener('storage', function(e) {
+	if(e.key === 'notes') {
+		notes = JSON.parse(e.newValue)
+		renderNotes(notes, filters)	
+	}
 })
