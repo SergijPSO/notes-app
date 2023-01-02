64f64467f25764d428e77555b40fb066967971e2
Sergij Pankiv <pankivsergij97@gmail.com>
2023-01-02 16:33:47 +0200

    moment
    moment.js library was added.
    Also i as learning how to use moment.js and manipulate data

---
+++ date.js
@@ -0,0 +1,27 @@
+// Unix Epoch - January 1st 1970 00:00
+  
+// const now = new Date()
+// const timestemp = now.getTime()
+
+// const myDate = new Date(timestemp)
+// console.log(myDate.getFullYear())
+
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of month: ${now.getDate()}`)
+// console.log(`Hour of month: ${now.getHours()}`)
+// console.log(`Minutes of month: ${now.getMinutes()}`)
+// console.log(`Seconds of month: ${now.getSeconds()}`)
+
+// const oneDate = new Date('November 16 1989 00:00:00')
+// const twoDate = new Date()
+
+// const getOneDateTimeStamp = oneDate.getTime()
+// const getTwoDateTimeStamp = twoDate.getTime()
+
+// if(getOneDateTimeStamp > getOneDateTimeStamp) {
+// 	console.log(oneDate.toString())
+// } else if(getTwoDateTimeStamp < getOneDateTimeStamp) {	
+// 	console.log(twoDate.toString())
+// }
--- notes-app.js
+++ notes-app.js
@@ -28,10 +28,22 @@ document.querySelector('#filter-by').addEventListener('change', function(e) {
 	console.log(e.target.value)
 })
 
-
 window.addEventListener('storage', function(e) {
 	if(e.key === 'notes') {
 		notes = JSON.parse(e.newValue)
-		renderNotes(notes, filters)	
+		renderNotes(notes, filters)
 	}
 })
+
+const now = moment()
+now.add(1, 'week').subtract(20, 'days')
+
+console.log(now.format('MMMM Do, YYYY'))
+console.log(now.fromNow())
+const nowTimeStamp = now.valueOf()
+console.log(moment(nowTimeStamp).toString())
+
+const birthday = moment()
+birthday.year(1989)
+birthday.month(11)
+console.log(birtday.format('MMM D, YYYY'))
